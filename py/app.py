0191bbae55d0f57d36571dd3342dd74b155be6b7
Martin Medvec <martin.medvec@visma.com>
2024-11-21 14:53:06 +0100

    some new hooks

--- py/app.py
+++ py/app.py
@@ -1,10 +1,34 @@
 import os
+from pprint import pprint
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+    pprint(body)
+    event = body.get("event", {})
+    text = event.get("text", "")
+    user = event.get("user", "")
+    channel = event.get("channel", "")
+    app.client.chat_postMessage(channel=channel, text=f"You said: {text}")
+
+@app.command("/hi")
+def handle_hello_command(ack, respond, command):
+    # Acknowledge the command request
+    ack()
+    # Respond to the command
+    respond(f"Hello, <@{command['user_id']}>!")
 # Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
